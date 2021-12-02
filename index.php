ef5c3ebd2513b3b330e5878ed1e921d56719eb46
husainf5272 <husain.f5272@gmail.com>
2021-12-02 20:17:46 +0530

    Database Connectivity

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'database.php';
+$db = new Database;
+$conn = $db->getConnection();
+
+$sql = "SELECT * FROM USER";
+$res = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($res);
+
+echo "Hello World";
+?>
